14a49cd885e595b2e70532d1af45ba1ca13fe4df
Green-Cherry <2423964104@qq.com>
2018-05-20 23:52:06 +0800

    create project

---
+++ src/main/java/edu/nju/cookery/Application.java
@@ -0,0 +1,23 @@
+package edu.nju.cookery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableScheduling
+public class Application extends SpringBootServletInitializer{
+    public static  void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+}
