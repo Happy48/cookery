c935f5f5dcfad416f2644a4e01fccfbcd9ada985
NathanielLu <854806557@qq.com>
2018-06-25 00:38:44 +0800

    修改笔记详情方法

--- src/main/java/edu/nju/cookery/service/impl/NoteDetailServiceImpl.java
+++ src/main/java/edu/nju/cookery/service/impl/NoteDetailServiceImpl.java
@@ -5,13 +5,14 @@ import edu.nju.cookery.repository.*;
 import edu.nju.cookery.service.NoteDetailService;
 import edu.nju.cookery.util.JsonUtil;
 import edu.nju.cookery.vo.*;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class NoteDetailServiceImpl implements NoteDetailService {
@@ -42,61 +43,112 @@ public class NoteDetailServiceImpl implements NoteDetailService {
         Timestamp foodCreateTime = note.getCreatedTime();
         int foodCollect = collectRepository.findByNoteID(noteId).size();
 
-        List<MaterialVO> materialVOList = new ArrayList<>();//??????
-        String material = note.getMaterial().substring(1, note.getMaterial().length()-1);
-        //
+        List<MaterialVO> materialVOList = new ArrayList<>();
         List<String> name = new ArrayList<>();
         List<String> unit = new ArrayList<>();
-
-        String[] group = material.split(", ");
-        for(String sss:group){
-            if(!sss.endsWith("}")){
-                sss= sss + "}";
+        try {
+            JSONArray jsonObject = JSONArray.fromObject(note.getMaterial());
+            for (Iterator<Object> iterator = jsonObject.iterator(); iterator.hasNext();) {
+                int cnt = 0;
+                JSONObject job = (JSONObject) iterator.next();
+                Iterator<Object> it=job.keys();
+                while (it.hasNext()){
+                    if(cnt%2==0){
+                        name.add(job.get(it.next()).toString());
+                    }else{
+                        unit.add(job.get(it.next()).toString());
+                    }
+                    cnt++;
+                }
             }
-            if(!sss.startsWith("{")){
-                sss = "{"+sss;
-            }
-            Map<String, String> map = JsonUtil.fromString(sss);
-            if(map.get("name")!=null){
-                name.add(map.get("name"));
-            }else{
-                unit.add(map.get("unit"));
-            }
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
         for(int i=0;i<name.size();i++){
             MaterialVO materialVO = new MaterialVO(name.get(i), unit.get(i));
             materialVOList.add(materialVO);
         }
+//        String material = note.getMaterial().substring(1, note.getMaterial().length()-1);
+//        //
+//        List<String> name = new ArrayList<>();
+//        List<String> unit = new ArrayList<>();
+//
+//        String[] group = material.split(", ");
+//        for(String sss:group){
+//            if(!sss.endsWith("}")){
+//                sss= sss + "}";
+//            }
+//            if(!sss.startsWith("{")){
+//                sss = "{"+sss;
+//            }
+//            Map<String, String> map = JsonUtil.fromString(sss);
+//            if(map.get("name")!=null){
+//                name.add(map.get("name"));
+//            }else{
+//                unit.add(map.get("unit"));
+//            }
+//        }
+//        for(int i=0;i<name.size();i++){
+//            MaterialVO materialVO = new MaterialVO(name.get(i), unit.get(i));
+//            materialVOList.add(materialVO);
+//        }
         //
 
-        List<StepVO> stepVOList = new ArrayList<>();//??????
-        String step = note.getPractice().substring(1, note.getPractice().length()-1);;
-        //
-        List<String> img = new ArrayList<>();
+        List<StepVO> stepVOList = new ArrayList<>();
         List<String> id = new ArrayList<>();
+        List<String> img = new ArrayList<>();
         List<String> information = new ArrayList<>();
-
-        String[] groupS = step.split(", ");
-        for(String sss:groupS){
-            if(!sss.startsWith("{")){
-                sss = "{"+sss;
+        try {
+            com.alibaba.fastjson.JSONArray jarr = com.alibaba.fastjson.JSONArray.parseArray(note.getPractice());
+            for (Iterator<Object> iterator = jarr.iterator(); iterator.hasNext();) {
+                int cnt = 0;
+                com.alibaba.fastjson.JSONObject job = (com.alibaba.fastjson.JSONObject) iterator.next();
+                Set<String> s=job.keySet();
+                for (String string : s) {
+                    if(cnt%3 == 0){
+                        img.add(job.get(string).toString());
+                    }else if(cnt%3 == 1){
+                        information.add(job.get(string).toString());
+                    }else{
+                        id.add(job.get(string).toString());
+                    }
+                    cnt++;
+                }
             }
-            if(!sss.endsWith("}")){
-                sss= sss + "}";
-            }
-            Map<String, String> map = JsonUtil.fromString(sss);
-            if(map.get("img")!=null){
-                img.add(map.get("img"));
-            }else if(map.get("id")!=null){
-                id.add(map.get("id"));
-            }else{
-                information.add(map.get("information"));
-            }
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
         for(int i=0;i<id.size();i++){
             StepVO stepVO = new StepVO(Integer.parseInt(id.get(i)), information.get(i), img.get(i));
             stepVOList.add(stepVO);
         }
+//        String step = note.getPractice().substring(1, note.getPractice().length()-1);;
+//        //
+//        List<String> img = new ArrayList<>();
+//        List<String> id = new ArrayList<>();
+//        List<String> information = new ArrayList<>();
+//
+//        String[] groupS = step.split(", ");
+//        for(String sss:groupS){
+//            if(!sss.startsWith("{")){
+//                sss = "{"+sss;
+//            }
+//            if(!sss.endsWith("}")){
+//                sss= sss + "}";
+//            }
+//            Map<String, String> map = JsonUtil.fromString(sss);
+//            if(map.get("img")!=null){
+//                img.add(map.get("img"));
+//            }else if(map.get("id")!=null){
+//                id.add(map.get("id"));
+//            }else{
+//                information.add(map.get("information"));
+//            }
+//        }
+//        for(int i=0;i<id.size();i++){
+//            StepVO stepVO = new StepVO(Integer.parseInt(id.get(i)), information.get(i), img.get(i));
+//            stepVOList.add(stepVO);
+//        }
         //
 
         List<WorkVO> workVOList = new ArrayList<>();
@@ -136,4 +188,60 @@ public class NoteDetailServiceImpl implements NoteDetailService {
         NoteDetailVO noteDetailVO = new NoteDetailVO(foodTitle, foodPic, foodDesc, foodLikes, foodCreateTime, foodCollect, noteId, userVO, materialVOList, stepVOList, workVOList, commentVOList);
         return noteDetailVO;
     }
+
+//    public static void main(String[] args){
+////        String material = "[{'name': '芋头', 'unit': '一个'}, {'name': '五花肉', 'unit': '适量'}, {'name': '冰糖', 'unit': '适量'}, {'name': '姜片', 'unit': '适量'}, {'name': '八角', 'unit': '两个'}, {'name': '桂皮', 'unit': '适量'}, {'name': '料酒', 'unit': '适量'}]";
+////        List<String> name = new ArrayList<>();
+////        List<String> unit = new ArrayList<>();
+////        try {
+////            JSONArray jsonObject = JSONArray.fromObject(material);
+////            for (Iterator<Object> iterator = jsonObject.iterator(); iterator.hasNext();) {
+////                int ct = 0;
+////                JSONObject job = (JSONObject) iterator.next();
+////                Iterator<Object> it=job.keys();
+////                while (it.hasNext()){
+////                    //System.out.println(job.get(it.next()));
+////                    if(ct%2==0){
+////                        name.add(job.get(it.next()).toString());
+////                    }else{
+////                        unit.add(job.get(it.next()).toString());
+////                    }
+////                    ct++;
+////                }
+////            }
+////        } catch (JSONException e) {
+////            e.printStackTrace();
+////        }
+////        System.out.println(name);
+////        System.out.println(unit);
+//
+//        String step = "[{\"information\": \"?????????????????170???\", \"id\": 1, \"img\": \"http://127.0.0.1:10086/http:s2cdnxiachufangcomd4c524228c0011e6b87c0242ac110003_199w_301hjpgimageView22w300interlace1q90\"}, {\"information\": \"????\", \"id\": 2, \"img\": \"http://127.0.0.1:10086/http:s1cdnxiachufangcomd4e5554e8c0011e6b87c0242ac110003_200w_300hjpg@2o_50sh_1pr_1l_300w_90q_1wh\"}, {\"information\": \"????????????????\", \"id\": 3, \"img\": \"http://127.0.0.1:10086/http:s1cdnxiachufangcomd5096b648c0011e6a9a10242ac110002_202w_300hjpg@2o_50sh_1pr_1l_300w_90q_1wh\"}, {\"information\": \"??????2?????????????????\", \"id\": 4, \"img\": \"http://127.0.0.1:10086/http:s2cdnxiachufangcomd5287c488c0011e6a9a10242ac110002_201w_301hjpgimageView22w300interlace1q90\"}, {\"information\": \"???????????????\", \"id\": 5, \"img\": \"http://127.0.0.1:10086/http:s1cdnxiachufangcomd54343f28c0011e6b87c0242ac110003_203w_301hjpg@2o_50sh_1pr_1l_300w_90q_1wh\"}, {\"information\": \"??????????????????????\", \"id\": 6, \"img\": \"http://127.0.0.1:10086/http:s2cdnxiachufangcomd56837e88c0011e6b87c0242ac110003_201w_299hjpgimageView22w300interlace1q90\"}, {\"information\": \"???????????????\", \"id\": 7, \"img\": \"http://127.0.0.1:10086/http:s2cdnxiachufangcomd587764e8c0011e6a9a10242ac110002_201w_300hjpgimageView22w300interlace1q90\"}, {\"information\": \"??????????????????????????????????170????25?30????????????????????????????????\", \"id\": 8, \"img\": \"http://127.0.0.1:10086/http:s2cdnxiachufangcomd5a3aeea8c0011e6a9a10242ac110002_201w_298hjpgimageView22w300interlace1q90\"}]";
+//        List<String> id = new ArrayList<>();
+//        List<String> img = new ArrayList<>();
+//        List<String> information = new ArrayList<>();
+//        try {
+//            com.alibaba.fastjson.JSONArray jarr = com.alibaba.fastjson.JSONArray.parseArray(step);
+//            for (Iterator<Object> iterator = jarr.iterator(); iterator.hasNext();) {
+//                int cnt = 0;
+//                com.alibaba.fastjson.JSONObject job = (com.alibaba.fastjson.JSONObject) iterator.next();
+//                Set<String> s=job.keySet();
+//                for (String string : s) {
+//                    if(cnt%3 == 0){
+//                        img.add(job.get(string).toString());
+//                    }else if(cnt%3 == 1){
+//                        information.add(job.get(string).toString());
+//                    }else{
+//                        id.add(job.get(string).toString());
+//                    }
+//                    cnt++;
+//                }
+//            }
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println(id);
+//        System.out.println(img);
+//        System.out.println(information);
+//    }
 }
